270e05b636c73fb57b7ddf9c27c744804d134f86
Autthawigorn <twiggyramirez8@gmail.com>
2024-03-10 09:41:20 +0700

    Create checkpoint3.swift

---
+++ checkpoint3.swift
@@ -0,0 +1,23 @@
+//
+//  checkpoint3.swift
+//  
+//
+//  Created by Autthawigorn Yortpiboot on 10/3/2567 BE.
+//
+
+import Foundation
+
+for number in 1...100 {
+    let fiz:Bool = number.isMultiple(of: 3)
+    let buz:Bool = number.isMultiple(of: 5)
+    switch (fiz, buz) {
+    case (true, true):
+        print("FizBuz")
+    case (true, false):
+        print("Fiz")
+    case (false, true):
+        print("Buz")
+    default:
+        print(number)
+    }
+}
