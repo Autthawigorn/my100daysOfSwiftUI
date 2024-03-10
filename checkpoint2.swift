38334dc3fb9aa4e1f0fc561df2e3b85e69bc1f8a
Autthawigorn <twiggyramirez8@gmail.com>
2024-03-10 10:04:24 +0700

    Create checkpoint2.swift

---
+++ checkpoint2.swift
@@ -0,0 +1,14 @@
+//
+//  checpoint2.swift
+//  
+//
+//  Created by Autthawigorn Yortpiboot on 10/3/2567 BE.
+//
+
+import Foundation
+
+let myArray = ["Art", "Zeen", "Zoon", "Nut", "Art", "Zoon"]
+
+print (myArray.count)
+
+print (Set(myArray).count)
