e861f45e4f84a55d38b720311d529bb1b83ff6c3
Autthawigorn <twiggyramirez8@gmail.com>
2024-03-10 10:35:12 +0700

    Create checkpoint1.swift

---
+++ checkpoint1.swift
@@ -0,0 +1,14 @@
+//
+//  checkpoint1.swift
+//  
+//
+//  Created by Autthawigorn Yortpiboot on 10/3/2567 BE.
+//
+
+import Foundation
+
+let celsius:Int = 30
+
+let fahrenheit = (celsius * 9)/5 + 32
+
+print("\(celsius)°C , \(fahrenheit)°F")
