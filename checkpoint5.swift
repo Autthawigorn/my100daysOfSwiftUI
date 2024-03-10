65c706da2b1fc425be66b0edb91be511c03c0140
Autthawigorn <twiggyramirez8@gmail.com>
2024-03-10 09:20:00 +0700

    Initial commit

---
+++ checkpoint5.swift
@@ -0,0 +1,16 @@
+//
+//  Checkpoint5.swift
+//  
+//
+//  Created by Autthawigorn Yortpiboot on 10/3/2567 BE.
+//
+
+import Foundation
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+
+let sortedResult = luckyNumbers.sorted {$0 < $1}
+let filteredNumbers = sortedResult.filter { !$0.isMultiple(of: 2) }
+let mappedStrings:[()] = filteredNumbers.map {print("\($0) is a lucky number")}
+
