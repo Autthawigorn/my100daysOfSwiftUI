68527759f137bc17429c83158f800537fb2e569a
Autthawigorn <twiggyramirez8@gmail.com>
2024-04-02 10:31:55 +0700

    Word Scramble 1st push

---
+++ Word Scramble/Word Scramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Word Scramble
+//
+//  Created by Autthawigorn Yortpiboot on 2/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Autthawigorn Yortpiboot on 2/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
