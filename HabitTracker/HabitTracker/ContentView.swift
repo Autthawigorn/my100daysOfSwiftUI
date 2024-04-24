79185f3d16c01a9f643e25be252feaec87f8a460
Autthawigorn <twiggyramirez8@gmail.com>
2024-04-24 21:00:47 +0700

    .

---
+++ HabitTracker/HabitTracker/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Autthawigorn Yortpiboot on 24/4/2567 BE.
+//
+
+import SwiftUI
+
+struct Activities {
+    
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabitTracker/HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Autthawigorn Yortpiboot on 24/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
