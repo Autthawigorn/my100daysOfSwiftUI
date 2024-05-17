24116a03926c103b00168b9f9a597880c47fc76e
Autthawigorn <twiggyramirez8@gmail.com>
2024-05-17 16:24:55 +0700

    test Binding

---
+++ Test@Binding/TestBinding/TestBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBindingApp.swift
+//  TestBinding
+//
+//  Created by Autthawigorn Yortpiboot on 17/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TestBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
