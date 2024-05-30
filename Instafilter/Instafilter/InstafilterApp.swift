9b35d431c61c4028b7e90cc808526b176ef48c01
Autthawigorn <twiggyramirez8@gmail.com>
2024-05-30 22:07:27 +0700

    finish Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Autthawigorn Yortpiboot on 26/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
