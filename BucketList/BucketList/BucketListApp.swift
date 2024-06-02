506ecd41d171cff8163eab73865a0f9c1b12f284
Autthawigorn <twiggyramirez8@gmail.com>
2024-06-02 10:38:11 +0700

    BucketList

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Autthawigorn Yortpiboot on 30/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
